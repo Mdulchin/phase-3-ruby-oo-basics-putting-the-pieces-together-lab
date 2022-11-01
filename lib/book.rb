f4c70d14f7e5328014101dc0957d70e861518e73
madelinedulchin <madeline.dulchin@gmail.com>
2022-11-01 11:59:04 -0400

    commit tree 2d3c9f4a843c103099649d25a4e65c926b297ba1

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+attr_reader :title, :author, :page_count, :genre
+attr_writer :author, :page_count, :genre
+
+
+def initialize(title)
+    @title = title
+end
+
+def turn_page
+  puts  "Flipping the page...wow, you read fast!"
+end
+
+end
 
 
